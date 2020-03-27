0f4c8b4cd80daa143b30e843fb51261059a5261a
DenilsonDonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-03-27 17:27:24 -0300

    Fechar teclado ao clicar fora dos edittext

--- app/src/main/java/com/ads/appgm/LoginActivity.java
+++ app/src/main/java/com/ads/appgm/LoginActivity.java
@@ -1,12 +1,14 @@
 package com.ads.appgm;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ads.appgm.util.MaskEditUtil;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,13 +17,31 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText inputCPF = findViewById(R.id.editTextCPF);
+        TextInputEditText inputCPF = findViewById(R.id.editTextCPF);
         inputCPF.addTextChangedListener(MaskEditUtil.mask(inputCPF,MaskEditUtil.FORMAT_CPF));
+        inputCPF.setOnFocusChangeListener(this::onFocusChange);
+
+        TextInputEditText inputPassword = findViewById(R.id.editTextPassword);
+        inputPassword.setOnFocusChangeListener(this::onFocusChange);
+
 
         MaterialButton btnLogin = findViewById(R.id.buttonLogin);
         btnLogin.setOnClickListener(v -> {
             setResult(RESULT_OK);
             finish();
         });
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus) {
+            hideKeyboard(v);
+        }
     }
 }
