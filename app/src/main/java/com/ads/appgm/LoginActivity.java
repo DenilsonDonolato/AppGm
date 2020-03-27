c175e6bd3eb18ff56d84422102fab81f62c76e0d
DenilsonDonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-03-27 15:54:59 -0300

    Design inicial da tela de login

--- app/src/main/java/com/ads/appgm/LoginActivity.java
+++ app/src/main/java/com/ads/appgm/LoginActivity.java
@@ -1,21 +1,24 @@
 package com.ads.appgm;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import com.ads.appgm.util.MaskEditUtil;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = findViewById(R.id.buttonLogin);
+        EditText inputCPF = findViewById(R.id.editTextCPF);
+        inputCPF.addTextChangedListener(MaskEditUtil.mask(inputCPF,MaskEditUtil.FORMAT_CPF));
+
+        MaterialButton btnLogin = findViewById(R.id.buttonLogin);
         btnLogin.setOnClickListener(v -> {
             setResult(RESULT_OK);
             finish();
