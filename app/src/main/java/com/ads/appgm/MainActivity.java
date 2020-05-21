89568ad94d50384c4e906662b449961e2d1ee0b9
denilsondonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-05-21 17:17:44 -0300

    Início de implementar a funcção de pânico

--- app/src/main/java/com/ads/appgm/MainActivity.java
+++ app/src/main/java/com/ads/appgm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ads.appgm;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -7,15 +8,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.ads.appgm.clickListeners.ButtonPanic;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 
 public class MainActivity extends AppCompatActivity {
+    private MaterialButton panicButton;
+    private SwitchMaterial panicFunction;
+
+    private TransitionDrawable transAnimButFlash;
+
+    boolean panicButtonStatus = false;
+    int btnAnimTime = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
+
+        panicFunction = findViewById(R.id.panic_function);
+        panicButton =  findViewById(R.id.buttonPanic);
+
+        transAnimButFlash = (TransitionDrawable) panicButton.getBackground();
+        transAnimButFlash.resetTransition();
+
         setSupportActionBar(myToolbar);
     }
 
@@ -26,5 +43,49 @@ public class MainActivity extends AppCompatActivity {
         panic.setOnClickListener(new ButtonPanic());
 
         super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isPanicQuickServiceRunning()) {
+            PanicQuick.getInstance().registerTorchieManagerListener(this);
+            if (this.isPanicOn()) {
+                this.setPanicButtonStatus(this.isPanicOn());
+            }
+        }
+        panicFunction.setChecked(isPanicQuickServiceRunning());
+    }
+
+    @Override
+    protected void onPause() {
+        if (this.isPanicQuickServiceRunning()) {
+            PanicQuick.getInstance().unregisterTorchieManagerListener();
+        } else if (this.isPanicOn()) {
+            this.toggleTorch(null);
+        }
+        super.onPause();
+        super.onPause();
+    }
+
+    private void setPanicButtonStatus(boolean enabled) {
+        panicButtonStatus = enabled;
+        if (panicButtonStatus) {
+            transAnimButFlash.startTransition(btnAnimTime);
+        } else {
+            transAnimButFlash.reverseTransition(btnAnimTime);
+        }
+    }
+
+    private boolean isPanicOn() {
+        if (isPanicQuickServiceRunning()) {
+            return PanicQuick.getInstance().getPanicStatus();
+        } else {
+            return PanicManager.getInstance(SettingsUtils.getPanicSource(this), true).getStatus();
+        }
+    }
+
+    private boolean isPanicQuickServiceRunning() {
+        return PanicQuick.getInstance() != null;
     }
 }
