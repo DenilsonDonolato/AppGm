f5e4e0245291b7659f72f95dfe79e743d0963a12
denilsondonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-06-22 17:49:11 -0300

    Layout de configurações

--- app/src/main/java/com/ads/appgm/SettingsActivity.java
+++ app/src/main/java/com/ads/appgm/SettingsActivity.java
@@ -6,20 +6,27 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.ads.appgm.databinding.SettingsActivityBinding;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private SettingsActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        binding = SettingsActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment())
+                .replace(binding.settings.getId(), new SettingsFragment())
                 .commit();
+        setSupportActionBar(binding.settingsToolbar.getRoot());
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        binding.settingsToolbar.getRoot().setNavigationOnClickListener(view -> onBackPressed());
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
