62e25424fc18a3699fe128cdfea58e54345554d3
denilsondonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-10-12 19:08:56 -0300

    Teste carrosel

--- app/src/main/java/com/ads/appgm/help/HelpActivity.java
+++ app/src/main/java/com/ads/appgm/help/HelpActivity.java
@@ -1,13 +1,18 @@
 package com.ads.appgm.help;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.ads.appgm.R;
 import com.ads.appgm.databinding.ActivityHelpBinding;
-import com.synnapps.carouselview.ImageListener;
+import com.jama.carouselview.CarouselView;
+import com.jama.carouselview.CarouselViewListener;
+import com.jama.carouselview.enums.IndicatorAnimationType;
+import com.jama.carouselview.enums.OffsetType;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -26,17 +31,30 @@ public class HelpActivity extends AppCompatActivity {
         binding = ActivityHelpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.carouselHelp.setPageCount(helpImages.length);
+//        binding.carouselHelp.setPageCount(helpImages.length);
+//
+//        binding.carouselHelp.setImageListener(imageListener);
+//
+//        setSupportActionBar(binding.settingsToolbar.getRoot());
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
+//        binding.settingsToolbar.getRoot().setNavigationOnClickListener(view -> onBackPressed());
 
-        binding.carouselHelp.setImageListener(imageListener);
-
-        setSupportActionBar(binding.settingsToolbar.getRoot());
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-        binding.settingsToolbar.getRoot().setNavigationOnClickListener(view -> onBackPressed());
+        binding.carouselView.setSize(helpImages.length);
+        binding.carouselView.setResource(R.layout.carousel_image);
+        binding.carouselView.setAutoPlay(false);
+        binding.carouselView.setIndicatorAnimationType(IndicatorAnimationType.THIN_WORM);
+        binding.carouselView.setCarouselOffset(OffsetType.CENTER);
+        binding.carouselView.setCarouselViewListener((view, position) -> {
+            // Example here is setting up a full image carousel
+            ImageView imageView = view.findViewById(R.id.carouselImage);
+            imageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(),helpImages[position],getTheme()));
+        });
+        // After you finish setting up, show the CarouselView
+        binding.carouselView.show();
     }
 
-    ImageListener imageListener = (position, imageView) -> imageView.setImageResource(helpImages[position]);
+//    ImageListener imageListener = (position, imageView) -> imageView.setImageResource(helpImages[position]);
 }
